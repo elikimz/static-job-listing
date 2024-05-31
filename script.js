cf8d90a137032287d6404527fac78511fecfb7a9
elijah -kimani <elijahkimani1293@gmail.com>
2024-05-31 11:07:33 +0300

    modified

--- script.js
+++ script.js
@@ -2,21 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const jobListingsContainer = document.querySelector('.job-listings');
     const filtersContainer = document.querySelector('.filters');
 
-    
+
     fetch('data.json')
         .then(response => response.json())
         .then(data => {
             const allJobs = data;
             let filteredJobs = allJobs;
 
+        
             const renderJobListings = (jobs) => {
                 jobListingsContainer.innerHTML = '';
                 jobs.forEach(job => {
                     const jobListing = document.createElement('div');
                     jobListing.className = 'job-listing';
                     jobListing.innerHTML = `
-                        <h2>${job.position}</h2>
-                        <p><strong>Company:</strong> ${job.company}</p>
+                        <div class="header">
+                            <img src="${job.logo}" alt="${job.company} logo">
+                            <div>
+                                <h2>${job.position}</h2>
+                                <p><strong>Company:</strong> ${job.company}</p>
+                            </div>
+                        </div>
                         <p><strong>Location:</strong> ${job.location}</p>
                         <p><strong>Contract:</strong> ${job.contract}</p>
                         <p><strong>Posted:</strong> ${job.postedAt}</p>
@@ -29,6 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             };
 
+    
             const renderFilters = () => {
                 const roles = [...new Set(allJobs.map(job => job.role))];
                 const levels = [...new Set(allJobs.map(job => job.level))];
